6c01b4dabd01f9d47f4c68df74a031c660fe1485
Pooja Gupta <poojageetagupta@gmail.com>
2018-10-18 20:10:21 -0500

    updated package.json and config database

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
-// 27017 is the default port number.  
+// 27017 is the default port number.
 module.exports = {
-    database: 'mongodb://localhost:27017/iitsummaries'
+    database: 'mongodb://54.184.252.108:27017/iitsummaries'
+    //database: 'mongodb://localhost:27017/iitsummaries'
 }
